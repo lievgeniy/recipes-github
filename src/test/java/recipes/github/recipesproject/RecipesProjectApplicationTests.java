4a62e7108d8d39ad7926245881ec1caba0c8a72c
Li Evgeniy <lievgueni@yahoo.fr>
2021-09-23 10:14:33 +0200

    Recipes project created

---
+++ src/test/java/recipes/github/recipesproject/RecipesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package recipes.github.recipesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
